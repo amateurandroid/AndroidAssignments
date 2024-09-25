46b8c443ccba3154caa98f9a6657554c2965e3d0
amateurandroid <nsfwishaandhillon@gmail.com>
2024-09-25 19:56:04 -0400

    did the bonus navigation thing in the assignment

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidassignments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void Back(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void Forward(View view) {
+        Intent intent = new Intent(this, ListItemsActivity.class);
+        startActivity(intent);
     }
 }
